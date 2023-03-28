78fe218a51c9021710e9dda4a2f9697b0dcd990d
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2023-03-28 11:27:46 -0400

    1.6.0

--- ds-utils.psm1
+++ ds-utils.psm1
@@ -1,3 +1,19 @@
 ('public') | Foreach-Object {
 	Get-ChildItem -Path $(Join-Path -Path $PSScriptRoot -ChildPath $_) -Filter "*.ps1" | Foreach-Object { . $_.FullName }
 }
+if (-not (Get-Alias -Name "grep")) {
+	New-Alias -Name "grep" -Value Invoke-Grep
+	Write-Host "Define alias: grep"
+}
+if (-not (Get-Alias -Name "sed")) {
+	New-Alias -Name "sed" -Value Invoke-Sed
+	Write-Host "Define alias: sed"
+}
+if (-not (Get-Alias -Name "msgbox")) {
+	New-Alias -Name "msgbox" -Value Invoke-DialogMsgBox
+	Write-Host "Define alias: msgbox"
+}
+if (-not (Get-Alias -Name "myip")) {
+	New-Alias -Name "myip" -Value Get-PublicIPv4Address
+	Write-Host "Define alias: myip"
+}
